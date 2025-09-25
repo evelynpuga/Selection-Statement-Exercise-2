25b0acf6a1cb30be0e637d0e9bfe0ab12f46f34c
Evelyn Puga <pugaevelyn0@gmail.com>
2025-09-25 14:26:36 -0700

    completed exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I don't like Math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules!");
+                    break;
+                case "p.e.":
+                case "pe":
+                case "physical education":
+                    Console.WriteLine("P.E. is fun");
+                    break;
+                case "history":
+                    Console.WriteLine("History is boring!");
+                    break;
+                case "social studies":
+                    Console.WriteLine("I like Social Studies too!");
+                    break;
+                default:
+                    Console.WriteLine("I like that too.");
+                    break;
+            }
         }
     }
 }
